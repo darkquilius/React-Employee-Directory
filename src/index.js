98f7ee4c090ea6f6ee776b84021660ba1f0348c4
Andrew Reeves <andrew.reeves.coding.bootcamp@gmail.com>
2020-07-21 10:53:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from "./App"
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <App /> ,
+ReactDOM.render( <React.StrictMode>
+		<App />
+	</React.StrictMode>,
     document.getElementById('root')
 );
 
